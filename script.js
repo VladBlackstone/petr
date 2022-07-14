61bad17badc55c37ca2df31735278c2d39251e98
vladpn <vladpn91@gmail.com>
2022-07-14 13:45:16 +0300

    films

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 "use strict";
 
+let numberOfFilms = prompt("Сколько фильмо вы уже посомтрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+
+
+
 // alert('hello');
 
 // let result = confirm("are u here?");
@@ -16,17 +41,17 @@
 // } else {
 //     alert("zaxodi")
 // };
-var answer = {
-    data: "тут данные ",
-};
-answer.age = prompt("age?","");
-if (answer.age < 88) {
-    alert("пошел нахуй отсюда!");
-    close();
-    stop(script);
-};
+// var answer = {
+//     data: "тут данные ",
+// };
+// answer.age = prompt("age?","");
+// if (answer.age < 88) {
+//     alert("пошел нахуй отсюда!");
+//     close();
+//     stop(script);
+// };
 
-answer.name = prompt("name?","");
-answer.lastname = prompt("lastname?","");
+// answer.name = prompt("name?","");
+// answer.lastname = prompt("lastname?","");
 
-document.write(`<div id='cent'><font color='white'><h3>${answer.data}${answer.name}а:</font><br>возраст: ${answer.age}<br>имя: ${answer.name}<br>фамилия:${answer.lastname}</h3></div>`);
+// document.write(`<div id='cent'><font color='white'><h3>${answer.data}${answer.name}а:</font><br>возраст: ${answer.age}<br>имя: ${answer.name}<br>фамилия:${answer.lastname}</h3></div>`);
